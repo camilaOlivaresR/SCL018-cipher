92e5947d5f60d90cd50ae19dc1db0f2abe1ebae9
camila <lamica.olivares@gmail.com>
2022-02-24 11:30:14 -0300

    funcion limpiar input

--- src/cipher.js
+++ src/cipher.js
@@ -2,60 +2,55 @@
 // toma dos parametros el offset y una letra
 function cipherChar(offset, character) {
     //declaro constante para minusculas segun posiicon en tabla ascii
-    const universalCase = character >= 65 && character <= 122;
+    const characterLowerCase = character >= 97 && character <= 122;
     //declaro variable para mayusculas segun posisicion en la tabla ascii
-    //const characterUpperCase = character >= 65 && character <= 90;
+    const characterUpperCase = character >= 65 && character <= 90;
 
     //si se cumple cualquiera de las condiciones entras al if
-    //if(characterLowerCase || characterUpperCase ) {
+    if (characterLowerCase || characterUpperCase) {
 
-    //le asigno el valor 26 a la variable modulo, por la cantidad de caracteres del alfabeto esp
-    let abc = 26;
-    // declaro la variable ascii y le asigno valor 65 
-    let AasciiTab = 65;
+        //le asigno el valor 26 a la variable modulo, por la cantidad de caracteres del alfabeto esp
+        let modulo = 26;
+        // declaro la variable ascii y le asigno valor 65 
+        let ascii = 65;
 
-    //si se cumple la condicion que sea letra minuscula
-    // if(characterLowerCase) {
-    // le asigno 97 a la variable ascii
-    //AasciiTab = 97;
+        //si se cumple la condicion que sea letra minuscula
+        if (characterLowerCase) {
+            // le asigno 97 a la variable ascii
+            ascii = 97;
+        }
+        //declaro variable charModulo
+        // que en este caso el charModulo toma el valor de la formula
+        let charModulo = ((character - ascii) + offset) % modulo;
+        //si el valor es menor que 0
+        if (charModulo < 0) {
+            //charmodulo tomara el sgte valor
+            charModulo = charModulo + modulo;
+        }
+        //entonces nos retorna un string especifico al valor que tome
+        // string.fromcharcodeat es un metodo que retorna un string
+        return String.fromCharCode((charModulo + ascii));
+    } else {
+        //si el valor no cumple las condicioes nos retorna el mismo caracter
+        return String.fromCharCode(character);
+    }
 }
-
-//declaro variable charModulo
-// que en este caso el charModulo toma el valor de la formula
-let charResiduo = ((character - AasciiTab) + offset) % abc;
-//si el valor es menor que 0
-if (charResiduo < 0) {
-    //charmodulo tomara el sgte valor
-    charResiduo = charResiduo + abc;
-}
-//entonces nos retorna un string especifico al valor que tome
-// string.fromcharcodeat es un metodo que retorna un string
-//((charModulo resultado de la formula+ascii))
-return String.fromCharCode((charResiduo + AasciiTab));
-}
-else {
-    //si el valor no cumple las condicioes nos retorna el mismo caracter
-    return String.fromCharCode(character);
-}
-}
-
 //declaro la funcion cipherString
 //PARA CIFARAR TODA LA CADENA DE TEXTO
 //recibe como parametros el offset y string
 function cipherString(offset, string) {
-    //exepciones o errores
+
     if (typeof offset !== "number") {
         throw new TypeError("error en valor de offset");
     }
-
     if (typeof string !== "string") {
         throw new TypeError("error en valor de string");
     }
 
     // se guarda el resultado del cifrado
     //inicializar una variable vacia de string
-    //que almacenara el resultado, se guardara un resultado
-    let result = "";
+    //que almacenara el resultado
+    let cipherResult = "";
 
     // se inicia un ciclo de iteracion que recorre todas las letras o string
     for (let i = 0; i < string.length; i++) {
@@ -63,12 +58,11 @@ function cipherString(offset, string) {
         //esta almacena la posicion numerica entera de cada letra del string
         const character = string.charCodeAt(i);
         // ahora cipherResult ira almacenando cada letra del string cifrada
-        result += cipherChar(offset, character);
+        cipherResult += cipherChar(offset, character);
     }
     //nos retorna el contenido total de la variable cipherResult
-    return result;
+    return cipherResult;
 }
-
 //declaro la constante cipher
 const cipher = {
     //el metodo encode declara una funcion que toma 2 parametros
@@ -82,5 +76,4 @@ const cipher = {
         return cipherString(-offset, string);
     }
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import cipher from './cipher.js';
-
 //declaro la variable boton codificar con id codebutton que es un elemento del Dom//
-const botonCodificar= document.getElementById("botonCodificar"); 
+const botonCodificar = document.getElementById("botonCodificar");
 const botonDecodificar = document.getElementById("botonDecodificar");
 // declaro la variable texto sin codificar con id textOriginal que es un elemento del Dom//
 const textOriginal = document.getElementById("textOriginal");
 const textCifrado = document.getElementById("textCifrado");
 //declaro variable shiftText que va buscar el id shift//
-const shiftNum = document.getElementById("shift");
+const shiftText = document.getElementById("shift");
 
 
 
@@ -16,24 +15,19 @@ const shiftNum = document.getElementById("shift");
 //luego se gatilla el el segundo parametro la funcion clickCode
 botonCodificar.addEventListener("click", clickCode);
 botonDecodificar.addEventListener("click", clickDecode);
-
-
-
 //declaro funcion clickCode
 function clickCode() {
     //declaro constante offset toma el valor del shiftText y lo convierte en numero entero
     // parseInt es una funcion que parsea o analiza un string y retorna un entero
-    const offset = parseInt(shiftNum.value);
+    const offset = parseInt(shiftText.value);
     // declaro constante que almacena el valor del texto original
-    const inputEntrada = textOriginal.value;
+    const firstInputText = textOriginal.value;
     //el valor de texto cifrado recibe el metodo encode aplicado a cipher
-    textCifrado.value = cipher.encode(offset, inputEntrada);
-}   
-
-function clickDecode() {
-    const offset = parseInt(shiftNum.value);
-    const inputSalida = textOriginal.value;
-   textCifrado.value = cipher.decode(offset, inputSalida);
+    textCifrado.value = cipher.encode(offset, firstInputText);
 }
 
-
+function clickDecode() {
+    const offset = parseInt(shiftText.value);
+    const secondInputText = textOriginal.value;
+    textCifrado.value = cipher.decode(offset, secondInputText);
+}
